47fe6370657a31ccb7824e98f214f0415df6a310
mdchia <u5494149@anu.edu.au>
2019-11-04 12:16:40 +1100

    reformatting

--- silva_consensus.py
+++ silva_consensus.py
@@ -5,15 +5,14 @@ import os
 
 from Bio import AlignIO
 from Bio.Align import AlignInfo
-from Bio import SeqIO
 
 # Usage: python silva_consensus.py <fasta alignments file>
 
 # Settings
 ambiguous_char = "X"
-out_filename = os.path.splitext(sys.argv[1])[0]+"_consensus.fasta"
-consensus_threshold = 0.6 # 0.5 = 50% of reads needed to call consensus
-min_reads = 3 # minimum number of reads required to assign the presence of a base
+out_filename = os.path.splitext(sys.argv[1])[0] + "_consensus.fasta"
+consensus_threshold = 0.6  # 0.5 = 50% of reads needed to call consensus
+min_reads = 3  # minimum number of reads required to assign the presence of a base
 
 # read in alignments
 alignment = AlignIO.read(sys.argv[1], 'fasta')
@@ -27,18 +26,18 @@ consensus = summary_align.gap_consensus(consensus_threshold, ambiguous=ambiguous
 consensus_seq = str(consensus)
 
 # get rid of bases that can't be called at confidence threshold
-consensus_seq = consensus_seq.replace(ambiguous_char,"")
+consensus_seq = consensus_seq.replace(ambiguous_char, "")
 
 # get rid of gaps
-consensus_seq = consensus_seq.replace("-","")
+consensus_seq = consensus_seq.replace("-", "")
 
 # for SILVA - get back to DNA
-consensus_seq = consensus_seq.replace("U","T")
+consensus_seq = consensus_seq.replace("U", "T")
 
 print("Writing file")
 
 # write out consensus
 with open(out_filename, 'w') as fasta:
     # create a fasta contig entry
-    fasta.write(">"+sys.argv[1]+"_consensus\n")
+    fasta.write(">" + sys.argv[1] + "_consensus\n")
     fasta.write(consensus_seq)
